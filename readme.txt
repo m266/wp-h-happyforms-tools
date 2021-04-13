e426926c2d068c2a750ef289d26f6f0775a8cb87
m266 <herbrand@mailbox.org>
2021-04-13 14:35:02 +0200

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,29 @@
+﻿=== WP H-HappyForms Tools ===
+Requires at least: 5.0
+Tested up to: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+Links in multi selection fields / Links in Mehrfachauswahl-Felder
+
+== Installation ==
+1. Upload 'wp-h-happyforms-tools'  to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure the plugin in Dashboard -> WP H-HappyForms Tools
+
+== Changelog ==
+Version 1.3.1
+1. Berichtigung
+
+Version 1.3
+1. Fehler beseitigt
+
+Version 1.2
+1. Code überarbeitet
+
+Version 1.1
+1. Check, ob Plugin "HappyForms" aktiv ist
+
+Version 1.0
+1. Initial Version
---
+++ wphhft.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name:       WP H-HappyForms Tools
+Plugin URI:        https://github.com/m266/wp-h-happyforms-tools
+Description:       Links in multi selection fields / Links in Mehrfachauswahl-Felder
+Author:            Hans M. Herbrand
+Author URI:        https://web266.de
+Version:           1.3.1
+Date:              2021-04-13
+License:           GNU General Public License v2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/m266/wp-h-happyforms-tools
+ */
+
+// Externer Zugriff verhindern
+defined('ABSPATH') || exit();
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Check Plugin activ
+if (!function_exists('is_plugin_inactive')) {
+    require_once ABSPATH . '/wp-admin/includes/plugin.php';
+}
+
+// Check GitHub Updater aktiv
+// Anpassungen Plugin-Name und Funktions-Name vornehmen
+if (is_plugin_inactive('github-updater/github-updater.php')) {
+// E-Mail an Admin senden, wenn inaktiv
+    register_activation_hook(__FILE__, 'wphhft_activate'); // Funktions-Name anpassen
+    function wphhft_activate()
+    { // Funktions-Name anpassen
+        $subject = 'Plugin "WP H-HappyForms Tools"'; // Plugin-Name anpassen
+        $message = 'Bitte das Plugin "<a href="https://web266.de/tutorials/github/github-updater/">GitHub Updater</a>" herunterladen, installieren und aktivieren, <br />um weiterhin Updates zu erhalten!';
+        wp_mail(get_option("admin_email"), $subject, $message);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Allow HTML-Code for HappyForms at multi selection fields
+// Is Plugin HappyForms activ?
+if (!function_exists('is_plugin_active')) {
+    require_once ABSPATH . '/wp-admin/includes/plugin.php';
+}
+if (is_plugin_active('happyforms/happyforms.php')) {  //Plugin HappyForms is activ
+//  Change Strings in frontend-checkbox.php Zeile 34
+    $wphhft_string_orig = "<?php echo esc_attr( \$option['label'] ); ?>";
+    $wphhft_string_new = "<?php echo html_entity_decode( \$option['label'] ); ?>";
+
+    $wphhft_path_to_file = ABSPATH . 'wp-content/plugins/happyforms/core/templates/parts/frontend-checkbox.php';
+    $wphhft_file_contents = file_get_contents($wphhft_path_to_file); // Inhalt frontend-checkbox.php einlesen
+if(strpos($wphhft_file_contents, $wphhft_string_orig) !== false) { //  Original-String vorhanden?
+    $wphhft_file_contents = str_replace($wphhft_string_orig, $wphhft_string_new, $wphhft_file_contents);
+    file_put_contents($wphhft_path_to_file, $wphhft_file_contents); // Replace strings
+    add_filter('happyforms_part_frontend_template_path_checkbox', function ($wphhft_template) {
+        $wphhft_template = ABSPATH . 'wp-content/plugins/happyforms/core/templates/parts/frontend-checkbox.php';
+        return $wphhft_template;
+    });
+}
+} else {
+    register_activation_hook(__FILE__, 'wphhft_inactiv'); // Funktions-Name anpassen
+    function wphhft_inactiv() //Plugin HappyForms is inactiv
+    { // Funktions-Name anpassen
+        $subject = 'Plugin "WP H-HappyForms Tools"'; // Plugin-Name anpassen
+        $message = 'Bitte das Plugin "<a href="https://de.wordpress.org/plugins/happyforms/">HappyForms</a>" installieren und aktivieren!';
+        wp_mail(get_option("admin_email"), $subject, $message);
+    }
+}
+?>
